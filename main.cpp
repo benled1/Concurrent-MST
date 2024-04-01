dcc05781c8f859a6c912c62dd235655ad57c5c6f
Sunil <sunilthemann@gmail.com>
2024-04-01 16:33:59 -0700

    Starting csv parsing

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include "utils/graph.h"
 
 int main(int argc, char *argv[]) {
-    Graph graph1("inputGraphs/bitcoin.csv");
+    Graph graph1("inputGraphs/bitcoin.txt");
+    cout << "List of edges:" << endl;
+    for (const Edge& edge : graph1.edges) {
+        cout << "Node " << edge.first << " to Node " << edge.second << ", Weight: " << edge.weight << endl;
+    }
     return 0;
 }
--- utils/graph.h
+++ utils/graph.h
@@ -24,23 +24,81 @@ struct Edge;
 
 class Vertex {
 public:
+    int index;
     vector<Edge> edges;
 };
 
 struct Edge {
-    Vertex* to;     // Pointer to the destination vertex
-    Vertex* from;   // Pointer to the source vertex
+public:
+    Vertex* first;     // Pointer to the destination vertex
+    Vertex* second;   // Pointer to the source vertex
     int weight;
 };
 
 class Graph {
 public:
     int V; // No. of vertices in graph
-    vector<vector<int>> adjMatrix;
     vector<Edge> edges; // Edges in the graph
-
+    vector<Vertex> vertices;
     // Create a graph from an input file
     Graph(string filename){
+        ifstream file(filename);
+        if (!file.is_open()) {
+            cerr << "Error opening file: " << filename << endl;
+        }
 
+        string line;
+        while (getline(file, line)) {
+            istringstream iss(line);
+            Edge edge;
+            int firstIndex, secondIndex, weight;
+            char comma;
+            if (iss >> firstIndex >> comma >> secondIndex >> comma >> weight) {
+                Vertex* vertexFirst = findOrCreateVertex(firstIndex);
+                Vertex* vertexSecond = findOrCreateVertex(secondIndex);
+                // Only add an edge if that pairing doesnt already have one
+                // This is to convert directed graphs to undirected
+                if(!edgeExists(*vertexFirst, *vertexSecond)){
+                    edges.push_back({vertexFirst, vertexSecond, weight});
+                    addEdgeToVertex(vertexFirst, vertexSecond, edge);
+                }
+            } else {
+                cerr << "Error parsing line: " << line << endl;
+            }
+        }
+
+        file.close();
+    }
+private:
+
+    // This is to see if we need to make a new vertex or not when created edges
+    Vertex* findOrCreateVertex(int index){
+        for(Vertex& v : vertices){
+            if(v.index == index){
+                return &v;
+            }
+        }
+        // Need to make a new vertex
+        Vertex newVertex = {index, {}};
+        vertices.push_back(newVertex);
+        return &vertices.back();
+    }
+
+    // Fill the individual vertex arrays as they need to keep track of neighbours
+    void addEdgeToVertex(Vertex* v1, Vertex* v2, Edge e){
+        v1->edges.push_back(e);
+        v2->edges.push_back(e);
+    }
+
+    // Check for duplicates
+    bool edgeExists(Vertex& v1, Vertex& v2){
+        // We only need to check 1 vertex as it will have to hold this pairing
+        for (Edge& edge : v1.edges) {
+            if ((edge.first == &v1 && edge.second == &v2) ||
+                (edge.first == &v2 && edge.second == &v1)) {
+                return true;
+            }
+        }
+        return false;
     }
 };
