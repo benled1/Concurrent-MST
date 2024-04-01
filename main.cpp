e2c38038712c7c01f02be332f696d6b2f72fd47d
Sunil <sunilthemann@gmail.com>
2024-04-01 15:30:27 -0700

    Adding some graph stuff

---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include "utils/graph.h"
+
+int main(int argc, char *argv[]) {
+    Graph graph1("inputGraphs/bitcoin.csv");
+    return 0;
+}
--- utils/graph.h
+++ utils/graph.h
@@ -1,4 +1,10 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
 #include <vector>
+#include <map>
+#include <unordered_set>
+#include <algorithm>
 
 /*
 Edges
@@ -8,18 +14,33 @@ Vertex
 - Holds a list of edges
 - ID to identify 
 Graph
-- 
+- Adjacency List?
+- Root node?
 */
 
+using namespace std;
+
+struct Edge;
+
 class Vertex {
-    public:
-        Vertex* neighbours;
-        
+public:
+    vector<Edge> edges;
+};
+
+struct Edge {
+    Vertex* to;     // Pointer to the destination vertex
+    Vertex* from;   // Pointer to the source vertex
+    int weight;
 };
 
 class Graph {
-    public:
-        int V; // No. of vertices in graph
-        vector<vector<pair<int, int>>> adjList;  // Pointer to an array containing adjacency lists
-        vector<vector<pair<int, int>>> adjWeights;  // Pointer to an array containing adjacency weights
+public:
+    int V; // No. of vertices in graph
+    vector<vector<int>> adjMatrix;
+    vector<Edge> edges; // Edges in the graph
+
+    // Create a graph from an input file
+    Graph(string filename){
+
+    }
 };
