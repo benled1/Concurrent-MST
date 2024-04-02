81d174c05c57f9b39bc81c78cde13df6c2d0c68b
Sunil <sunilthemann@gmail.com>
2024-04-01 20:32:30 -0700

    added Smaller testData

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(int argc, char *argv[]) {
     cout << "Starting Parsing" << endl;
-    Graph graph1("inputGraphs/bitcoin.csv");
+    Graph graph1("inputGraphs/testData.csv");
     cout << "List of edges:" << endl;
     for (const Edge& edge : graph1.edges) {
         cout << "Node " << edge.first << " to Node " << edge.second << ", Weight: " << edge.weight << endl;
