5662d0bb1766d9557d67e2521994ef37e40e6b3b
Sunil <sunilthemann@gmail.com>
2024-04-01 20:16:20 -0700

    Changed vertex to int

--- utils/graph.h
+++ utils/graph.h
@@ -20,22 +20,10 @@ Graph
 
 using namespace std;
 
-struct Edge;
-
-struct Vertex {
-public:
-    int index;
-    vector<Edge> edges;
-    friend ostream& operator<<(ostream& os, const Vertex& v) {
-        os << "Vertex index - " << v.index << ", Number of edges - " << v.edges.size();
-        return os;
-    }
-};
-
 struct Edge {
 public:
-    Vertex* first;     // Pointer to the destination vertex
-    Vertex* second;   // Pointer to the source vertex
+    int first;     // Pointer to the destination vertex
+    int second;   // Pointer to the source vertex
     int weight;
 };
 
@@ -43,7 +31,6 @@ class Graph {
 public:
     int V; // No. of vertices in graph
     vector<Edge> edges; // Edges in the graph
-    vector<Vertex> vertices;
     // Create a graph from an input file
     Graph(string filename){
         ifstream file(filename);
@@ -57,14 +44,11 @@ public:
             int firstIndex, secondIndex, weight;
             char comma;
             if (iss >> firstIndex >> comma >> secondIndex >> comma >> weight) {
-                Vertex* vertexFirst = findOrCreateVertex(firstIndex);
-                Vertex* vertexSecond = findOrCreateVertex(secondIndex);
                 // Only add an edge if that pairing doesnt already have one
                 // This is to convert directed graphs to undirected
-                cout << &vertexFirst << " : " << &vertexSecond << endl;
-                if(!edgeExists(*vertexFirst, *vertexSecond)){
-                    edges.push_back({vertexFirst, vertexSecond, weight});
-                    addEdgeToVertex(vertexFirst, vertexSecond, edges.back());
+                if(!edgeExists(firstIndex, secondIndex)){
+                    edges.push_back({firstIndex, secondIndex, weight});
+                    // addEdgeToVertex(vertexFirst, vertexSecond, edges.back());
                 }
             } else {
                 cerr << "Error parsing line: " << line << endl;
@@ -74,40 +58,15 @@ public:
         file.close();
     }
 private:
-
-    // This is to see if we need to make a new vertex or not when created edges
-    Vertex* findOrCreateVertex(int index){
-        if(!vertices.empty()){
-            for(const Vertex& v : vertices){
-                if(v.index == index){
-                    return &const_cast<Vertex&>(v);
-                }
-            }
+    bool edgeExists(int v1, int v2){
+        if(edges.empty()){
+            return false;
         }
-        // Need to make a new vertex
-        vertices.push_back({index, {}});
-        return &vertices.back();
-    }
-
-    // Fill the individual vertex arrays as they need to keep track of neighbours
-    void addEdgeToVertex(Vertex* v1, Vertex* v2, Edge e){
-        v1->edges.push_back(e);
-        v2->edges.push_back(e);
-    }
-
-    // Check for duplicates
-    bool edgeExists(Vertex& v1, Vertex& v2){
-        cout << v1 << " : " << v2 << endl;
-        if(v1.edges.empty()){
-            return true;
-        }
-        for (const Edge& edge : v1.edges) {
-            if ((edge.first == &v1 && edge.second == &v2) ||
-                (edge.first == &v2 && edge.second == &v1)) {
+        for (Edge e : edges){
+            if((e.first == v1 && e.second == v2) || (e.first == v2 && e.second == v1)){
                 return true;
             }
         }
-        cout << "Set Free" << endl;
         return false;
     }
 };
