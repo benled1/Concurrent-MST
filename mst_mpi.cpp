985c72644b6f1d513b2b2b5dfa595d3191920e84
Ben <benledingham1@gmail.com>
2024-04-07 16:43:07 -0700

    removed comments changed makefile

--- mst_mpi.cpp
+++ mst_mpi.cpp
@@ -7,15 +7,6 @@
 
 using namespace std;
 
-// // implement a disjoint set data structure here
-// // https://www.geeksforgeeks.org/disjoint-set-data-structures/
-
-// // TODO:
-// //     implement a DisjointSet look at the link above
-// //     implement the findMinOutGoingEdge
-// //     implement the full algorithm but in a serial fashion like written below
-// //     add the distributed portions by partitioning the edge set and adding broadcasting
-
 bool isLeavingEdge(Edge& edge, vector<int> connected_vertices) {
     if (find(connected_vertices.begin(), connected_vertices.end(), edge.vertex1->id) == connected_vertices.end() ||
         find(connected_vertices.begin(), connected_vertices.end(), edge.vertex2->id) == connected_vertices.end()) {
@@ -55,8 +46,6 @@ vector<Edge> distributedPrims(Graph& inputGraph) {
     vector<Edge> mst;
     vector<Edge> min_edges;
 
-    // find the minimum outgoing edge for each subgraph (this iteration, each subgraph is a vertex)
-    // if there is a minimum outgoing edge, push it onto the min_edges vector
     for(int i=0; i<ds.size;i++) {
         if(ds.find(i)!=i) {
             continue;
@@ -72,30 +61,19 @@ vector<Edge> distributedPrims(Graph& inputGraph) {
     // sort the min_edges vector which now has a min_edge for every subgraph in the subgraph_set
     sort(min_edges.begin(), min_edges.end());
 
-    for (Edge edge: min_edges) {
-        cout<<edge.vertex1->id<<"-"<<edge.vertex2->id<<": Weight="<<edge.weight<<endl;
-    }
-
-
-
 
     // loop while there are still edges in the min_edges vector
     while(!min_edges.empty()){
-        // for every min edge, check if it connects two disjoint subgraphs, if yes, merge the two it connects
-        // if a min edge merges two subgraphs, push it to our final mst.
         for (int i=0; i<min_edges.size();i++) {
             if(ds.find(min_edges[i].vertex1->id) != ds.find(min_edges[i].vertex2->id)) {
                 ds.merge(min_edges[i].vertex1->id, min_edges[i].vertex2->id);
                 mst.push_back(min_edges[i]);
-                // cout<<edge.vertex1->id<<"-"<<edge.vertex2->id<<": Weight="<<edge.weight<<endl;
             }
         }
         
         // clear the min_edges
         min_edges.clear();
 
-        // refill the min_edges with the new min_leaving edges on the new merged subgraphs
-        // there should be a min edge for every new subgraph in the subgraphs_set
         for(int i=0; i<ds.size;i++) {
             if(ds.find(i)!=i) {
                 continue;
@@ -113,7 +91,6 @@ vector<Edge> distributedPrims(Graph& inputGraph) {
         for (Edge edge: min_edges) {
             cout<<edge.vertex1->id<<"-"<<edge.vertex2->id<<": Weight="<<edge.weight<<endl;
         }
-        // repeat above until we find no min outgoing edges due to the subgraphs_set only having one subgraph.
     }
 
     return mst;
@@ -130,8 +107,10 @@ int main(int argc, char *argv[]){
             std::cout << " with weight: " << edge->weight << std::endl;
         }
     }
+
     vector<Edge> mst = distributedPrims(g);
-    
+
+    cout<<"MST:"<<endl;
     for(Edge edge: mst) {
         cout<<edge.vertex1->id<<"-"<<edge.vertex2->id<<": Weight="<<edge.weight<<endl;
     }
