6a35cd8e966286f0e2187c808f32b83b4489277f
Ben Ledingham <benledingham1@gmail.com>
2024-04-11 21:34:26 -0700

    added the submission folder shell

--- mpi.sh
+++ mpi.sh
@@ -4,6 +4,6 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=4
 #SBATCH --partition=slow
-#SBATCH --mem=10G
+#SBATCH --mem=1G
 
 srun ./mst_mpi /home/bledingh/CMP431-slurm/final_project/CMPT431-Project/inputGraphs/connected_graph.csv true
