c93f182db7cea2584a69adeb54c9548e69143ed5
Sunil <sunilthemann@gmail.com>
2024-04-12 23:47:43 -0700

    swapped the algorithm files

--- mst_thread.cpp
+++ mst_thread.cpp
@@ -1,151 +1,105 @@
 #include "utils/graph.h"
-#include <condition_variable>
+#include "utils/disjoint_set.h"
+#include <cassert>
+#include <algorithm>
+#include <string>
+#include <limits>
+#include <mutex>
 #include <thread>
 #include <mutex>
-#include <queue>
-#include <functional>
-#include <atomic>
-#include <algorithm>
 
 using namespace std;
-std::mutex graphMutex;
-std::vector<std::mutex> vertexMutexes;
-std::vector<std::mutex> mergeMutexes;
+atomic<bool> mstDone(false);
+mutex pqMtx;
+mutex mstMtx;
+mutex dsMtx;
 
-void mergeTree(int thread1, int thread2, std::vector<std::vector<Edge>>& threadMSTVec, std::vector<bool>& restartVec){
-    if(thread1 < thread2){
-        cout << "SOMETHING WENT WRONG WITH THREAD ORDERING IN MERGE" << endl;
-        int temp = thread1;
-        thread1 = thread2;
-        thread2 = temp;
-    }
-    // Only merge bigger threads tree into smaller thread
-    threadMSTVec[thread2].insert(threadMSTVec[thread2].end(), threadMSTVec[thread1].begin(), threadMSTVec[thread1].end());
-    std::sort(threadMSTVec[thread2].begin(), threadMSTVec[thread2].end());
-    std::unordered_set<Edge, EdgeHasher> edgesSet;
-    for(Edge e : threadMSTVec[thread2]){
-        e.vertex1->color = thread2;
-        e.vertex2->color = thread2;
-        edgesSet.insert(e);
-    }
-    bool hasDuplicates = edgesSet.size() != threadMSTVec[thread2].size();
-    cout << "Duplicates: " << (hasDuplicates ? "Yes" : "No") << endl;
-    if(hasDuplicates){
-        // Remake vector if it has dupes
-        threadMSTVec[thread2].clear();
-        threadMSTVec[thread2].insert(threadMSTVec[thread2].end(), edgesSet.begin(), edgesSet.end());
-    }
-    // Remove from old thread mst
-    threadMSTVec[thread1].clear();
-    restartVec[thread1] = true;
-}
-
-void mstThread(int tid, Graph &g, std::vector<std::vector<Edge>>& threadMSTVec, std::vector<bool>& restartVec){
-    while(true)
-    {
-        priority_queue<Edge> edge_pq;
-        graphMutex.lock();
-        Vertex* starting_vertex = g.getRandomUnvisitedVertex(tid);
-        graphMutex.unlock();
-        cout << "Thread " << tid << " grabbed " << starting_vertex->id << endl;
-        if(starting_vertex == nullptr){
-            return;
-        }
-        // Add all adjacent edges to the PQ
-        for (Edge* edge : starting_vertex->edges) {
-            edge_pq.push(*edge);
-        }
-        // Keep going until we have no more to go through
-        while(!edge_pq.empty()){
-            if(restartVec[tid] == true){
-                // Kill the thread 
-                cout << "Thread #" << tid << " has ended" << endl;
-                return;
+void mstThread(int tid, Graph& g, DisjointSet& ds, vector<Edge>& mst, priority_queue<Edge>& pq){
+    // terminate at some condition
+    while(true){
+        pqMtx.lock();
+        if(pq.empty())
+        {
+            if(mst.size() == g.V - 1){
+                mstDone = true;
             }
-            Edge current_edge = edge_pq.top();
-            edge_pq.pop();
-            Vertex* targetVertex;
-            if(current_edge.vertex1->color == -1){
-                targetVertex = current_edge.vertex1;
+            pqMtx.unlock();
+            if (mstDone){ 
+                break;
             }
             else{
-                targetVertex = current_edge.vertex2;
+                continue;
             }
-
-            vertexMutexes[targetVertex->id].lock();
-            int targetColor = targetVertex->color;
-            if(targetColor == -1){
-                //Unvisited
-                targetVertex->color = tid;
-                
-                mergeMutexes[tid].lock();
-                threadMSTVec[tid].push_back(current_edge);
-                mergeMutexes[tid].unlock();
-
-                for (Edge* edge : targetVertex->edges) {
-                    if(edge->vertex1->color == -1 || edge->vertex2->color == -1 ){
-                        edge_pq.push(*edge);
-                    }
+        }
+        Edge current_edge = pq.top();
+        pq.pop();
+        pqMtx.unlock();
+        dsMtx.lock();
+        int id1 = current_edge.vertex1->id;
+        int id2 = current_edge.vertex2->id;
+        if(ds.find(id1) != ds.find(id2)){
+            mstMtx.lock();
+            cout << current_edge.vertex1->id << "--" << current_edge.vertex2->id << " " << tid << endl; 
+            mst.push_back(current_edge);
+            mstMtx.unlock();
+            ds.merge(id1, id2); 
+            dsMtx.unlock();
+            pqMtx.lock();
+            for (Edge* e : current_edge.vertex1->edges) {
+                int otherVertexId = (e->vertex1->id == id1) ? e->vertex2->id : e->vertex1->id;
+                dsMtx.lock();
+                if (ds.find(id1) != ds.find(otherVertexId))
+                {
+                    pq.push(*e);
                 }
-                // Unblock everyone
-                vertexMutexes[targetVertex->id].unlock();
+                dsMtx.unlock();
             }
-            else if(targetColor != tid){
-                // Visited by another thread
-                int visitedThread = targetColor;
-                // Kill the largest thread
-                if(visitedThread > tid){
-                    mergeMutexes[tid].lock();
-                    mergeMutexes[visitedThread].lock();
-                    mergeTree(visitedThread, tid, std::ref(threadMSTVec), std::ref(restartVec));
-
-                    mergeMutexes[tid].unlock();
-                    mergeMutexes[visitedThread].unlock();
+            for (Edge* e : current_edge.vertex2->edges) {
+                int otherVertexId = (e->vertex1->id == id2) ? e->vertex1->id  : e->vertex2->id ;
+                dsMtx.lock();
+                if (ds.find(id2) != ds.find(otherVertexId))
+                {
+                    pq.push(*e);
                 }
-                else if (tid > visitedThread){
-                    mergeMutexes[visitedThread].lock();
-                    mergeMutexes[tid].lock();
-                    
-                    mergeTree(tid, visitedThread, std::ref(threadMSTVec), std::ref(restartVec));
-                    
-                    mergeMutexes[visitedThread].unlock();
-                    mergeMutexes[tid].unlock();
-                }
-                // Unlock the mutex
-                vertexMutexes[targetVertex->id].unlock();
+                dsMtx.unlock();
             }
-            else if (targetColor == tid){
-                //Visited by self
-                // Unlock the mutex
-                vertexMutexes[targetVertex->id].unlock();
-            }
-        } 
-        break;  
+            pqMtx.unlock();
+        }
+        else{
+            dsMtx.unlock();
+        }
     }
+
 }
 
-vector<Edge> serialPrims(Graph g, int nthreads){
-    size_t v = (size_t) g.V;
-    std::vector<std::mutex> list1(v);
-    vertexMutexes.swap(list1);
-    std::vector<std::mutex> list2(nthreads);
-    mergeMutexes.swap(list2);
-    std::thread threads[nthreads];
-    std::vector<bool> restartVec(nthreads);
-    std::vector<std::vector<Edge>> threadMSTVec(nthreads);
-    cout << "Starting Threads" << endl;
-    for(int i = 0; i < nthreads; i++){
-       threads[i] = std::thread(mstThread, i, std::ref(g), std::ref(threadMSTVec), std::ref(restartVec));
-        cout << i << " Thread started" << endl;
+vector<Edge> serialPrims(Graph& inputGraph, int n_threads) {
+    clock_t start = clock();
+
+    // init the disjoint set, final ms and threads
+    DisjointSet ds(inputGraph.V);
+    vector<Edge> mst;
+    thread threads[n_threads];
+
+    // Create the initial values for the pq
+    priority_queue<Edge> edge_pq;
+    Vertex* starting_vertex = inputGraph.vertices[0];
+    for (Edge* edge : starting_vertex->edges) {
+        edge_pq.push(*edge);
     }
-    for(int i = 0; i < nthreads; i++){
+    cout << "PQ Size" << edge_pq.size() << endl;
+    for(int i = 0; i < n_threads; ++i){
+        cout << "Thread #" << i << " started" << endl;
+        threads[i] = thread(mstThread, i, std::ref(inputGraph), std::ref(ds), std::ref(mst), std::ref(edge_pq));
+    }
+    for(int i = 0; i < n_threads; ++i){
         threads[i].join();
     }
-    cout << "Output MST" << endl;
-    for(Edge e : threadMSTVec[0]){
-        cout << e.vertex1->id << " --- " << e.vertex2->id << ", weight: " << e.weight << endl;
-    }
-    vector<Edge> mst;
+
+    clock_t end = clock();
+    // convert to milliseconds
+    double elapsed = double(end - start) / CLOCKS_PER_SEC * 1000; 
+    cout << "Elapsed time: " << elapsed << " ms" <<endl;
+
     return mst;
 }
 
@@ -171,7 +125,6 @@ int main(int argc, char *argv[]){
     for(Edge edge: mst) {
         cout<<edge.vertex1->id<<"-"<<edge.vertex2->id<<": Weight="<<edge.weight<<endl;
     }
+
+    return 0;
 }
-
-    
-
