df88bcf53eee8c0773f5ec09523f869bc9900570
Sunil <sunilthemann@gmail.com>
2024-04-14 16:44:30 -0700

    Updated the README. CreateGraphs now works in python3

--- README.md
+++ README.md
@@ -1,5 +1,54 @@
 # CMPT431-Project
 
+## How to run
+### Makefile
+To use the make file
+```
+make all
+```
+This will make all the files needed to run the different implemenations of MST. The different executables.<br>
+To make the files individually
+```
+make mst_serial
+```
+```
+make mst_thread
+```
+```
+make mst_mpi
+```
+### C++ files
+To run those files
+```
+./mst_serial <graph path> <bool for printing logs>
+```
+```
+./mst_thread <n tasks> <graph path> <bool for printing logs>>
+```
+```
+./mst_mpi <graph path> <bool for printing logs>
+```
+The graph path is the path to whatever `csv` file is being used as the input graph. We have a folder called `inputGraphs` that holds all our test graphs and a script to creat more.
+### Create additional graphs
+To create a new graph use the python file `createGraph` like so
+```
+python3 inputGraphs/createGraph.py
+```
+After that follow the input steps from the terminal. This will create a new connected graph that can be used with the `c++` files.
+### Running in slurm
+To run the files in `Slurm`, the bash scripts are needed. They are all created with the file names `serial.sh, thread.sh,mpi.sh`. They run each of the different implementations. There are preset settings but they can be changed as needed. <br>
+Example on how to run
+```
+sbatch serial.sh
+``` 
+All of the scripts output to a text file labelled `test_<implementation>.txt` with implemenation being the chosen implementation. 
+### Evaluation script
+To test and compare all the implementations at once, use the script `evaluate_procs.py`. This will run all three in Slurm and then output the recorded times from the `.txt.` files. If you want to adjust the testing parameters do not change this script, instead change the `.sh` files. <br>
+Run command
+```
+python3 evaluate_procs.py
+```
+This will take a variable amount of time so let it run until all jobs are done. Note: make sure to check that no slurm job failed if the script is taking a considerable amount of time, this will result in an infinite loop of waiting for an update.
 ## Pseudo-Code
 
 ### Serial
--- inputGraphs/createGraph.py
+++ inputGraphs/createGraph.py
@@ -1,5 +1,6 @@
 import random
 import csv
+import os  # Import os to handle directory creation
 
 class Graph:
     def __init__(self, vertices):
@@ -18,32 +19,35 @@ class Graph:
         self.add_edge(self.vertices - 1, 0, random.randint(1, 100))
 
     def write_to_csv(self, filename):
-        with open(filename, 'w') as csvfile:
+        with open(filename, 'w', newline='') as csvfile:  # 'newline=''' to avoid extra new lines in csv files
             writer = csv.writer(csvfile)
             for edge in self.edges:
                 writer.writerow(edge)
 
-if __name__ == "__main__":
-    file_name = raw_input("Enter the filename for the graph: ")
-    num_vertices = int(input("Enter the number of vertices: "))
-    num_edges = int(input("Enter the number of edges (greater than or equal to vertices): "))
+file_name = input("Enter the filename for the graph: ")  # Changed raw_input to input for Python 3
+num_vertices = int(input("Enter the number of vertices: "))
+num_edges = int(input("Enter the number of edges (greater than or equal to vertices): "))
 
-    if num_edges < num_vertices:
-        print("Number of edges must be greater than or equal to the number of vertices.")
-    else:
-        graph = Graph(num_vertices)
-        graph.generate_connected_graph()
-        # Generate remaining random edges
-        for _ in range(num_edges - num_vertices + 1):
-            v1 = random.randint(0, num_vertices - 1)
-            v2 = random.randint(0, num_vertices - 1)
-            while v1 == v2:  # Ensure no self-loops
-                v2 = random.randint(0, num_vertices - 1)
+if num_edges < num_vertices:
+    print("Number of edges must be greater than or equal to the number of vertices.")
+else:
+    graph = Graph(num_vertices)
+    graph.generate_connected_graph()
+
+    # Generate remaining random edges
+    additional_edges = num_edges - num_vertices + 1
+    while additional_edges > 0:
+        v1 = random.randint(0, num_vertices - 1)
+        v2 = random.randint(0, num_vertices - 1)
+        if v1 != v2:  # Ensure no self-loops
             weight = random.randint(1, 100)
             graph.add_edge(v1, v2, weight)
+            additional_edges -= 1
 
-        graph.write_to_csv("inputGraphs/" + file_name + ".csv")
-        print("CSV file created successfully.")
+    # Ensure the directory exists before writing
+    os.makedirs('inputGraphs', exist_ok=True)  # Create directory if it doesn't exist
+    full_path = os.path.join("inputGraphs", file_name + ".csv")
+    graph.write_to_csv(full_path)
+    print("CSV file created successfully.")
 
-# Your isConnected method in C++
 
